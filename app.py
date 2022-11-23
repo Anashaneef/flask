0fa9378454fbcb22e0914f874a8038c0848fec96
Anashaneef <anas.f.hanif@students.amikom.ac.id>
2022-11-23 14:51:09 +0700

    edit py file structure

--- app.py
+++ app.py
@@ -38,35 +38,36 @@ def coffee():
     if request.method == 'GET':
         return jsonify({'message': 'Hello World!'})
     if request.method == 'POST':
-        json_data = flask.request.json
-        aroma = json_data["aroma"]
-        acid = json_data["acid"]
-        body = json_data["body"]
-        flavor = json_data["flavor"]
-        aftertaste = json_data["aftertaste"]
-        float_features = [aroma, acid, body, flavor, aftertaste]
-        # return(float_features)
-        features = [np.array(float_features)]
-        prediction = model.predict(features)
+        return jsonify({'message': 'Posted!'})
+        # json_data = flask.request.json
+        # aroma = json_data["aroma"]
+        # acid = json_data["acid"]
+        # body = json_data["body"]
+        # flavor = json_data["flavor"]
+        # aftertaste = json_data["aftertaste"]
+        # float_features = [aroma, acid, body, flavor, aftertaste]
+        # # return(float_features)
+        # features = [np.array(float_features)]
+        # prediction = model.predict(features)
 
-        data = coffee[coffee['name'] == prediction.item()]
-        name = data['name'].values[0]
-        origin = data['origin'].values[0]
-        rating = data['rating'].values[0]
-        roaster = data['roaster'].values[0]
-        aroma = data['aroma'].values[0]
-        acid = data['acid'].values[0]
-        body = data['body'].values[0]
-        flavor = data['flavor'].values[0]
-        aftertaste = data['aftertaste'].values[0]
-        desc_1 = data['desc_1'].values[0]
-        desc_2 = data['desc_2'].values[0]
-        desc_3 = data['desc_3'].values[0]
+        # data = coffee[coffee['name'] == prediction.item()]
+        # name = data['name'].values[0]
+        # origin = data['origin'].values[0]
+        # rating = data['rating'].values[0]
+        # roaster = data['roaster'].values[0]
+        # aroma = data['aroma'].values[0]
+        # acid = data['acid'].values[0]
+        # body = data['body'].values[0]
+        # flavor = data['flavor'].values[0]
+        # aftertaste = data['aftertaste'].values[0]
+        # desc_1 = data['desc_1'].values[0]
+        # desc_2 = data['desc_2'].values[0]
+        # desc_3 = data['desc_3'].values[0]
 
-        #get recom
-        recom = app_recom(name)
+        # #get recom
+        # recom = app_recom(name)
 
-        return jsonify({'name': name, 'origin': origin, 'rating': str(rating), 'roaster': roaster, 'aroma': str(aroma), 'acid': str(acid), 'body': str(body), 'flavor': str(flavor), 'aftertaste': str(aftertaste), 'desc_1': desc_1, 'desc_2': desc_2, 'desc_3': desc_3, 'recom_name1': recom['name'].values[0], 'recom_origin1': recom['origin'].values[0], 'recom_rating1': str(recom['rating'].values[0]), 'recom_roaster1': recom['roaster'].values[0], 'recom_aroma1': str(recom['aroma'].values[0]), 'recom_acid1': str(recom['acid'].values[0]), 'recom_body1': str(recom['body'].values[0]), 'recom_flavor1': str(recom['flavor'].values[0]), 'recom_aftertaste1': str(recom['aftertaste'].values[0]), 'recom_desc_11': recom['desc_1'].values[0], 'recom_desc_21': recom['desc_2'].values[0], 'recom_desc_31': recom['desc_3'].values[0], 'recom_name2': recom['name'].values[1], 'recom_origin2': recom['origin'].values[1], 'recom_rating2': str(recom['rating'].values[1]), 'recom_roaster2': recom['roaster'].values[1], 'recom_aroma2': str(recom['aroma'].values[1]), 'recom_acid2': str(recom['acid'].values[1]), 'recom_body2': str(recom['body'].values[1]), 'recom_flavor2': str(recom['flavor'].values[1]), 'recom_aftertaste2': str(recom['aftertaste'].values[1]), 'recom_desc_12': recom['desc_1'].values[1], 'recom_desc_22': recom['desc_2'].values[1], 'recom_desc_32': recom['desc_3'].values[1], 'recom_name3': recom['name'].values[2], 'recom_origin3': recom['origin'].values[2], 'recom_rating3': str(recom['rating'].values[2]), 'recom_roaster3': recom['roaster'].values[2], 'recom_aroma3': str(recom['aroma'].values[2]), 'recom_acid3': str(recom['acid'].values[2]), 'recom_body3': str(recom['body'].values[2]), 'recom_flavor3': str(recom['flavor'].values[2]), 'recom_aftertaste3': str(recom['aftertaste'].values[2]), 'recom_desc_13': recom['desc_1'].values[2], 'recom_desc_23': recom['desc_2'].values[2], 'recom_desc_33': recom['desc_3'].values[2], 'recom_name4': recom['name'].values[3], 'recom_origin4': recom['origin'].values[3], 'recom_rating4': str(recom['rating'].values[3]), 'recom_roaster4': recom['roaster'].values[3], 'recom_aroma4': str(recom['aroma'].values[3]), 'recom_acid4': str(recom['acid'].values[3]), 'recom_body4': str(recom['body'].values[3]), 'recom_flavor4': str(recom['flavor'].values[3]), 'recom_aftertaste4': str(recom['aftertaste'].values[3]), 'recom_desc_14': recom['desc_1'].values[3], 'recom_desc_24': recom['desc_2'].values[3], 'recom_desc_34': recom['desc_3'].values[3], 'recom_name5': recom['name'].values[4], 'recom_origin5': recom['origin'].values[4], 'recom_rating5': str(recom['rating'].values[4]), 'recom_roaster5': recom['roaster'].values[4], 'recom_aroma5': str(recom['aroma'].values[4]), 'recom_acid5': str(recom['acid'].values[4]), 'recom_body5': str(recom['body'].values[4]), 'recom_flavor5': str(recom['flavor'].values[4]), 'recom_aftertaste5': str(recom['aftertaste'].values[4]), 'recom_desc_15': recom['desc_1'].values[4], 'recom_desc_25': recom['desc_2'].values[4], 'recom_desc_35': recom['desc_3'].values[4]})
+        # return jsonify({'name': name, 'origin': origin, 'rating': str(rating), 'roaster': roaster, 'aroma': str(aroma), 'acid': str(acid), 'body': str(body), 'flavor': str(flavor), 'aftertaste': str(aftertaste), 'desc_1': desc_1, 'desc_2': desc_2, 'desc_3': desc_3, 'recom_name1': recom['name'].values[0], 'recom_origin1': recom['origin'].values[0], 'recom_rating1': str(recom['rating'].values[0]), 'recom_roaster1': recom['roaster'].values[0], 'recom_aroma1': str(recom['aroma'].values[0]), 'recom_acid1': str(recom['acid'].values[0]), 'recom_body1': str(recom['body'].values[0]), 'recom_flavor1': str(recom['flavor'].values[0]), 'recom_aftertaste1': str(recom['aftertaste'].values[0]), 'recom_desc_11': recom['desc_1'].values[0], 'recom_desc_21': recom['desc_2'].values[0], 'recom_desc_31': recom['desc_3'].values[0], 'recom_name2': recom['name'].values[1], 'recom_origin2': recom['origin'].values[1], 'recom_rating2': str(recom['rating'].values[1]), 'recom_roaster2': recom['roaster'].values[1], 'recom_aroma2': str(recom['aroma'].values[1]), 'recom_acid2': str(recom['acid'].values[1]), 'recom_body2': str(recom['body'].values[1]), 'recom_flavor2': str(recom['flavor'].values[1]), 'recom_aftertaste2': str(recom['aftertaste'].values[1]), 'recom_desc_12': recom['desc_1'].values[1], 'recom_desc_22': recom['desc_2'].values[1], 'recom_desc_32': recom['desc_3'].values[1], 'recom_name3': recom['name'].values[2], 'recom_origin3': recom['origin'].values[2], 'recom_rating3': str(recom['rating'].values[2]), 'recom_roaster3': recom['roaster'].values[2], 'recom_aroma3': str(recom['aroma'].values[2]), 'recom_acid3': str(recom['acid'].values[2]), 'recom_body3': str(recom['body'].values[2]), 'recom_flavor3': str(recom['flavor'].values[2]), 'recom_aftertaste3': str(recom['aftertaste'].values[2]), 'recom_desc_13': recom['desc_1'].values[2], 'recom_desc_23': recom['desc_2'].values[2], 'recom_desc_33': recom['desc_3'].values[2], 'recom_name4': recom['name'].values[3], 'recom_origin4': recom['origin'].values[3], 'recom_rating4': str(recom['rating'].values[3]), 'recom_roaster4': recom['roaster'].values[3], 'recom_aroma4': str(recom['aroma'].values[3]), 'recom_acid4': str(recom['acid'].values[3]), 'recom_body4': str(recom['body'].values[3]), 'recom_flavor4': str(recom['flavor'].values[3]), 'recom_aftertaste4': str(recom['aftertaste'].values[3]), 'recom_desc_14': recom['desc_1'].values[3], 'recom_desc_24': recom['desc_2'].values[3], 'recom_desc_34': recom['desc_3'].values[3], 'recom_name5': recom['name'].values[4], 'recom_origin5': recom['origin'].values[4], 'recom_rating5': str(recom['rating'].values[4]), 'recom_roaster5': recom['roaster'].values[4], 'recom_aroma5': str(recom['aroma'].values[4]), 'recom_acid5': str(recom['acid'].values[4]), 'recom_body5': str(recom['body'].values[4]), 'recom_flavor5': str(recom['flavor'].values[4]), 'recom_aftertaste5': str(recom['aftertaste'].values[4]), 'recom_desc_15': recom['desc_1'].values[4], 'recom_desc_25': recom['desc_2'].values[4], 'recom_desc_35': recom['desc_3'].values[4]})
 
 def app_recom(label, similarity_data=cosine_sim_df, items=recom[['name', 'origin', 'rating', 'roaster', 'aroma', 'acid', 'body', 'flavor', 'aftertaste', 'desc_1', 'desc_2', 'desc_3']], k=5):
     # Retrieve data by using argpartition to partition indirectly along a given axis    
